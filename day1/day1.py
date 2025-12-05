60beb98cf8e3f88c87e9adc3be930581a407fd9b
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2025-12-05 08:31:15 -0600

    finished part 1 now onto part 2

--- day1/day1.py
+++ day1/day1.py
@@ -2,41 +2,68 @@
 mid = 50
 count = 0 
 
-file = open("input.txt", "r")
-line = file.readline()
+def save_value(line):
+    newLine = line[1:]
+    multiplier = 1
+    
+    if len (newLine) == 2:
+        return int(newLine) , 1
+    
+    else:
+        lastLine = newLine[-2:]
+        multiplier = newLine[:-2]
+    return int(lastLine) , int(multiplier)
 
-while line:
-    line = file.readline()
-    if line:
-        newVal = int(line[1:]) % 100
+
+with open("input.txt", "r") as file:
+
+    
+    for line in file:
+        if not line: 
+            continue
+    
+        line = line.strip()
+        
+        # newVal = int(line[1:]) % 100
+
+        newVal, multiplier = save_value(line)
+        newVal = int(newVal)
+        
         match line[0]:               
             case "L":
                 if mid - newVal < 0:
                     mid = 100 + (mid-newVal)
+                    count += multiplier
                 else: mid -= newVal
+                if mid == 0:
+                    count +=1
                 
-            case "R":
+            case "R": 
                 if mid + newVal > 99:
-                    mid = (mid+newVal) - 100
+                    mid = (newVal+mid) - 100
+                    count += multiplier
                 else: mid += newVal
-        if mid == 0:
-            count +=1               
-file.close()
+                if mid ==0:
+                    count +=1
+                
+        # if mid == 0:
+        #     count +=1               
+    file.close()
 
-print("count",count)
+    print("count",count)
 
-"""
-mid = 5
-L20
-5 - 20 <0 yes
-mid = 100 + -15 
-85
+    """
+    mid = 5
+    L20
+    5 - 20 <0 yes
+    mid = 100 + -15 
+    85
 
-Mid = 85
-R20
+    Mid = 85
+    R20
 
-85 + 20 > 99: yes
-mid = (85 + 20)- 100 
-5
+    85 + 20 > 99: yes
+    mid = (85 + 20)- 100 
+    5
 
-"""
+    """
